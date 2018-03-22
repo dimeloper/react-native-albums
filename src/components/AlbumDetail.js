e44a1df2824f731f78832e81c004ff33b52d9cfb
dkiri <dkiri@esailors.de>
2018-03-22 02:13:13 +0000

    Styling the album elements, added button.

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,17 +1,34 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
-const AlbumDetail = props => {
+const AlbumDetail = ({ album }) => {
+  const { title, artist, thumbnail_image, image } = album;
+  const {
+    headerContentStyle,
+    headerTitleStyle,
+    thumbnailContainerStyle,
+    thumbnailStyle,
+    albumArtworkStyle
+  } = styles;
   return (
     <Card>
       <CardSection>
-        <View />
-        <View style={styles.headerContentStyle}>
-          <Text>{props.album.title}</Text>
-          <Text>{props.album.artist}</Text>
+        <View style={thumbnailContainerStyle}>
+          <Image style={thumbnailStyle} source={{ uri: thumbnail_image }} />
         </View>
+        <View style={headerContentStyle}>
+          <Text style={headerTitleStyle}>{title}</Text>
+          <Text>{artist}</Text>
+        </View>
+      </CardSection>
+      <CardSection>
+        <Image style={albumArtworkStyle} source={{ uri: image }} />
+      </CardSection>
+      <CardSection>
+        <Button />
       </CardSection>
     </Card>
   );
@@ -21,6 +38,24 @@ const styles = {
   headerContentStyle: {
     flexDirection: 'column',
     justifyContent: 'space-around'
+  },
+  headerTitleStyle: {
+    fontSize: 18
+  },
+  thumbnailStyle: {
+    height: 50,
+    width: 50
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  },
+  albumArtworkStyle: {
+    height: 300,
+    flex: 1,
+    width: null
   }
 };
 
